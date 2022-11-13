cf6ee89a6c5366e02158d5e6f5bd6e847fc91bce
이빵주 <yungju9982@naver.com>
2022-11-14 03:28:15 +0900

    #3 [chore] 바인딩액티비티, 바인딩프래그먼트 생성

---
+++ app/src/main/java/com/sopt/aladinaos/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.aladinaos.presentation
+
+import android.os.Bundle
+import com.sopt.aladinaos.R
+import com.sopt.aladinaos.databinding.ActivityMainBinding
+import com.sopt.aladinaos.util.binding.BindingActivity
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
