003eecbfcd7f5c38661496c5871691c3061ef31f
b1urrrr <b1urrr@naver.com>
2022-11-15 03:27:10 +0900

    #10 [fix] fixed .gitignore untracked files

---
+++ app/src/main/java/com/sopt/aladinaos/presentation/detail/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.sopt.aladinaos.presentation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
