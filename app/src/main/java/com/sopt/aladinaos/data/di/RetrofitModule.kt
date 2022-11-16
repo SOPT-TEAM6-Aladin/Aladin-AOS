62e76810254059fda5f8256e93ed9855488d2428
이빵주 <yungju9982@naver.com>
2022-11-17 03:57:31 +0900

    #12 [fix] 로컬프로퍼티값 제거하고 상수로 변경

--- app/src/main/java/com/sopt/aladinaos/data/di/RetrofitModule.kt
+++ app/src/main/java/com/sopt/aladinaos/data/di/RetrofitModule.kt
@@ -1,6 +1,5 @@
 package com.sopt.aladinaos.data.di
 
-import com.sopt.aladinaos.BuildConfig
 import com.sopt.aladinaos.data.service.CartService
 import com.sopt.aladinaos.data.service.DetailService
 import com.sopt.aladinaos.data.service.HomeService
@@ -31,7 +30,7 @@ class RetrofitModule {
                         .newBuilder()
                         .addHeader(
                             HEADER_AUTHORIZATION,
-                            BuildConfig.DUMMY_ACCESS_TOKEN
+                            DUMMY_ACCESS_TOKEN
                         )
                         .addHeader(AUTHORIZATION, USER_ID)
                         .build()
@@ -57,7 +56,7 @@ class RetrofitModule {
         okHttpClient: OkHttpClient
     ): Retrofit =
         Retrofit.Builder()
-            .baseUrl(BuildConfig.ALADIN_URI)
+            .baseUrl(ALADIN_URI)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
@@ -78,5 +77,7 @@ class RetrofitModule {
         private const val HEADER_AUTHORIZATION = "Authorization"
         private const val AUTHORIZATION = "Authorization"
         private const val USER_ID = "1"
+        private const val ALADIN_URI = "aladin uri"
+        private const val DUMMY_ACCESS_TOKEN = "dummy access token"
     }
 }
