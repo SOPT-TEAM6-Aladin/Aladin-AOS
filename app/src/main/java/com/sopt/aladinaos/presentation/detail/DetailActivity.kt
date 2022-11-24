945dfdcd918b2f144c2c87b767e42b0737634873
b1urrrr <b1urrr@naver.com>
2022-11-25 03:54:55 +0900

    #22 [ui] 뒤로가기 및 장바구니 버튼 연결

--- app/src/main/java/com/sopt/aladinaos/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/sopt/aladinaos/presentation/detail/DetailActivity.kt
@@ -1,15 +1,31 @@
 package com.sopt.aladinaos.presentation.detail
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.sopt.aladinaos.R
 import com.sopt.aladinaos.databinding.ActivityDetailBinding
+import com.sopt.aladinaos.presentation.cart.CartActivity
+import com.sopt.aladinaos.util.binding.BindingActivity
 
-class DetailActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityDetailBinding
+class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        initBackBtnClickListener()
+        initCartBtnClickListener()
+    }
+
+    private fun initBackBtnClickListener() {
+        binding.btnDetailBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun initCartBtnClickListener() {
+        binding.btnDetailCart.setOnClickListener {
+            val toCart = Intent(this, CartActivity::class.java)
+            startActivity(toCart)
+        }
     }
 }
