51ac75bf3017722fbf7a961fc10f79a57c69783d
이빵주 <yungju9982@naver.com>
2022-11-25 21:56:19 +0900

    #24 [delete] 불필요한 코드 제거

--- app/src/main/java/com/sopt/aladinaos/data/repository/CartRepository.kt
+++ app/src/main/java/com/sopt/aladinaos/data/repository/CartRepository.kt
@@ -1,7 +1,6 @@
 package com.sopt.aladinaos.data.repository
 
 import com.sopt.aladinaos.data.entity.response.BaseResponse
-import com.sopt.aladinaos.data.entity.response.Book
 import com.sopt.aladinaos.data.entity.response.CartResponse
 
 interface CartRepository {
